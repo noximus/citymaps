0335c0b01f287778053b0cc0cb7c1a32efec8776
noximus <hrichardson83@gmail.com>
2015-03-07 16:56:35 -0500

    made the google api an updateable model

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,21 +9,15 @@
  */
 angular.module('devApp')
   .controller('MainCtrl', function ($scope,uiGmapGoogleMapApi) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.map = { center: { latitude: 49, longitude: -73 }, zoom: 8 };
+    $scope.updateLoc = function(latLoc,longLoc){
+    	//manual update for now
+    	
+
+    	console.log($scope.map.center.latitude);
+    }
     uiGmapGoogleMapApi.then(function(maps) {
 
     });
-    
- //    var map;
-	// function initialize() {
-	// 	console.log("running");
-	//   var mapOptions = {
-	//     zoom: 8,
-	//     center: new google.maps.LatLng(-34.397, 150.644)
-	//   };
-	//   map = new google.maps.Map(document.getElementById('map-canvas'),
-	//       mapOptions);
-	// }
 
-	// google.maps.event.addDomListener(window, 'load', initialize);
   });

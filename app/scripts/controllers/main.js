bff208f82ddf904069ec2c3649c491535b356c46
noximus <hrichardson83@gmail.com>
2015-03-08 12:27:39 -0400

    added markers to google map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,11 +10,7 @@
 angular.module('devApp')
   .controller('MainCtrl', function ($scope,uiGmapGoogleMapApi,cityMaps,$resource) {
   	// initial google map settings
-    $scope.map = { center: { latitude: 49, longitude: -78 }, zoom: 4, markers: {
-        latitude: '',
-        longitude: '',
-        title: ''
-      },tempMarkers:{}};
+    $scope.map = { center: { latitude: 49, longitude: -78 }, zoom: 12, markers: [],tempMarkers:{}};
     $scope.options = {scrollwheel: false};
     $scope.search = 'pizza in bushwick';
 
@@ -26,35 +22,31 @@ angular.module('devApp')
 		    if (status == google.maps.GeocoderStatus.OK) {
 		    	$scope.map.center.latitude = results[0].geometry.location.lat();
 		    	$scope.map.center.longitude = results[0].geometry.location.lng();
-
 		    	$scope.map.tempMarkers = cityMaps.get({ search_q: $scope.search, lat: $scope.map.center.latitude, lng: $scope.map.center.longitude }, function() {
-					// console.log($scope.map.markers.items[1].name);
-					// place cityMaps into array and push to google maps markers
-					// addMarkerToMap();
-					console.log($scope.map.tempMarkers);
+					addMarkerToMap();
 				});
 		    } else {
 		      // alert('Geocode was not successful for the following reason: ' + status);
 		    }
 		  });
     });
-    
-	
     // initalie map after
     uiGmapGoogleMapApi.then(function(maps) {
     	$scope.mapping = maps
     	console.log("uiGmapGoogleMapApi",maps)
     });
+    // add marker function
     function addMarkerToMap() {
-    	// console.log("addMarkerToMap",$scope.map.tempMarkers.items[1]);
-
     	for (var i = 0; i < $scope.map.tempMarkers.count; i++) {
-	    	var myLatlng = new google.maps.LatLng($scope.map.tempMarkers.items[i].lat,$scope.map.tempMarkers.items[i].lng);
-			// var marker = new google.maps.Marker({
-			// 	position: myLatlng,
-			// 	map: $scope.mapping
-			// });
-			// markers.push(marker);
+			var marker = new google.maps.Marker({
+	            idKey: i,
+	              coords: {
+	                latitude: $scope.map.tempMarkers.items[i].lat,
+	                longitude: $scope.map.tempMarkers.items[i].lon
+		        },
+		        title: $scope.map.tempMarkers.items[i].name
+		    })
+			$scope.map.markers.push(marker);
 		};
 	}
   });
